222a951fedc6adc8d352b91810a1377e087c416f
issler78 <132786398+Issler78@users.noreply.github.com>
2026-06-05 13:28:43 -0300

    first commit

---
+++ src/main/java/br/com/issler/azura_api/AzuraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.issler.azura_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/issler/azura_api/AzuraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.issler.azura_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
