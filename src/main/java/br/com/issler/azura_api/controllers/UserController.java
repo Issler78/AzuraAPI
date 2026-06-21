d25e5349dd9f93887f4da3e1d42f26169e74da0e
issler78 <132786398+Issler78@users.noreply.github.com>
2026-06-21 19:41:15 -0300

    feature: implement get my enrollments (by token)

--- src/main/java/br/com/issler/azura_api/controllers/UserController.java
+++ src/main/java/br/com/issler/azura_api/controllers/UserController.java
@@ -1,12 +1,16 @@
 package br.com.issler.azura_api.controllers;
 
 import br.com.issler.azura_api.database.models.EnrollmentEntity;
+import br.com.issler.azura_api.database.models.UserEntity;
 import br.com.issler.azura_api.dtos.CategoryResponse;
 import br.com.issler.azura_api.dtos.CourseResponse;
 import br.com.issler.azura_api.dtos.EnrollmentResponse;
 import br.com.issler.azura_api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +26,33 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{userId}/enrollments")
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.OK)
-    public Set<EnrollmentResponse> getEnrollments(@PathVariable UUID userId) throws Exception {
+    public Set<EnrollmentResponse> getEnrollmentsByUserId(@PathVariable UUID userId) throws Exception {
         Set<EnrollmentEntity> enrollments = userService.getEnrollments(userId);
+
+        return enrollments.stream().map(
+                enrollment -> EnrollmentResponse.builder()
+                        .id(enrollment.getId())
+                        .enrollmentDate(enrollment.getEnrollmentDate())
+                        .course(CourseResponse.builder()
+                                .id(enrollment.getCourse().getId())
+                                .title(enrollment.getCourse().getTitle())
+                                .description(enrollment.getCourse().getDescription())
+                                .category(CategoryResponse.builder()
+                                        .id(enrollment.getCourse().getCategory().getId())
+                                        .name(enrollment.getCourse().getCategory().getName())
+                                        .build())
+                                .build())
+                        .build()
+        ).collect(Collectors.toSet());
+    }
+
+    @GetMapping("/me/enrollments")
+    @PreAuthorize("hasRole('STUDENT')")
+    @ResponseStatus(HttpStatus.OK)
+    public Set<EnrollmentResponse> getMyEnrollments(@AuthenticationPrincipal UserEntity user) throws Exception {
+        Set<EnrollmentEntity> enrollments = userService.getEnrollments(user.getId());
 
         return enrollments.stream().map(
                 enrollment -> EnrollmentResponse.builder()
